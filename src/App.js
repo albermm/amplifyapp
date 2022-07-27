7280ba677f2a9136a9944d8cd027ab06a5f95d5d
albermm <albermm.78@gmail.com>
2022-07-27 15:51:08 +1200

    fix auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
+import "@aws-amplify/ui-react/styles.css";
 import './App.css';
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
+  return (
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
+
 
 function App() {
   return (
@@ -23,4 +45,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
