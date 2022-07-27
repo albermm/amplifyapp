af6f0024db692744bba4c4641d706a86a375933b
albermm <albermm.78@gmail.com>
2022-07-27 16:18:21 +1200

    ui-react styles updated

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-AmplifyClass.configure(config);
+import "@aws-amplify/ui-react/styles.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
